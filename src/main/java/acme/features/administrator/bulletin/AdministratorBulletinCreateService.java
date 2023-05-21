93064816bb34f6e8d515f773089c7c19c561445d
alepervaz <alepervaz@alum.us.es>
2023-05-21 13:19:03 +0200

    fix(update): fix bulletin validation past date in post

--- src/main/java/acme/features/administrator/bulletin/AdministratorBulletinCreateService.java
+++ src/main/java/acme/features/administrator/bulletin/AdministratorBulletinCreateService.java
@@ -1,29 +1,33 @@
 
 package acme.features.administrator.bulletin;
 
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import acme.entities.bulletin.Bulletin;
 import acme.framework.components.accounts.Administrator;
 import acme.framework.components.models.Tuple;
 import acme.framework.helpers.MomentHelper;
 import acme.framework.services.AbstractService;
 import acme.services.SpamService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 @Service
 public class AdministratorBulletinCreateService extends AbstractService<Administrator, Bulletin> {
 
 	// Constants -------------------------------------------------------------
-	protected static final String[] PROPERTIES = {
-			"moment", "title", "message", "flags", "link"
+	protected static final String[]				PROPERTIES	= {
+		"moment", "title", "message", "flags", "link"
 	};
 
 	// Internal state ---------------------------------------------------------
 
 	@Autowired
-	protected AdministratorBulletinRepository repository;
+	protected AdministratorBulletinRepository	repository;
 	@Autowired
-	protected SpamService spamDetector;
+	protected SpamService						spamDetector;
 
 	// AbstractService interface ----------------------------------------------
 
@@ -41,7 +45,6 @@ public class AdministratorBulletinCreateService extends AbstractService<Administ
 	@Override
 	public void load() {
 		Bulletin object;
-
 		object = new Bulletin();
 
 		super.getBuffer().setData(object);
@@ -52,7 +55,6 @@ public class AdministratorBulletinCreateService extends AbstractService<Administ
 		assert object != null;
 
 		super.bind(object, AdministratorBulletinCreateService.PROPERTIES);
-		object.setMoment(MomentHelper.getCurrentMoment());
 	}
 
 	@Override
@@ -60,29 +62,30 @@ public class AdministratorBulletinCreateService extends AbstractService<Administ
 		assert object != null;
 
 		boolean confirmation;
+		Date currentMoment;
 
 		if (!super.getBuffer().getErrors().hasErrors("confirmation")) {
 			confirmation = super.getRequest().getData("confirmation", boolean.class);
 			super.state(confirmation, "confirmation", "javax.validation.constraints.AssertTrue.message");
 		}
+		if (!super.getBuffer().getErrors().hasErrors("moment")) {
+			currentMoment = MomentHelper.deltaFromCurrentMoment(1, ChronoUnit.DAYS);
+			super.state(MomentHelper.isAfter(currentMoment, object.getMoment()), "moment", "javax.validation.constraints.Date");
+		}
 
 		// Spam validation
-		if (!super.getBuffer().getErrors().hasErrors("title")) {
+		if (!super.getBuffer().getErrors().hasErrors("title"))
 			super.state(this.spamDetector.validateTextInput(object.getTitle()), "title", "administrator.bulletin.form.error.spam.title");
-		}
-		if (!super.getBuffer().getErrors().hasErrors("message")) {
+		if (!super.getBuffer().getErrors().hasErrors("message"))
 			super.state(this.spamDetector.validateTextInput(object.getMessage()), "message", "administrator.bulletin.form.error.spam.message");
-		}
-		if (!super.getBuffer().getErrors().hasErrors("link")) {
+		if (!super.getBuffer().getErrors().hasErrors("link"))
 			super.state(this.spamDetector.validateTextInput(object.getLink()), "link", "administrator.bulletin.form.error.spam.link");
-		}
 
 	}
 
 	@Override
 	public void perform(final Bulletin object) {
 		assert object != null;
-		object.setMoment(MomentHelper.getCurrentMoment());
 		this.repository.save(object);
 	}
 
